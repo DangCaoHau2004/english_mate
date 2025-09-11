5c0239cb06f32547dc1af17d5b5247ca28924da5
Dang Cao Hau <caohauhau9@gmail.com>
2025-09-11 19:59:01 +0700

    update illustrations

--- lib/generated/assets.gen.dart
+++ lib/generated/assets.gen.dart
@@ -12,8 +12,60 @@ import 'package:flutter/widgets.dart';
 class $AssetsImagesGen {
   const $AssetsImagesGen();
 
+  /// Directory path: assets/images/illustrations
+  $AssetsImagesIllustrationsGen get illustrations =>
+      const $AssetsImagesIllustrationsGen();
+
   /// Directory path: assets/images/logo
   $AssetsImagesLogoGen get logo => const $AssetsImagesLogoGen();
+}
+
+class $AssetsImagesIllustrationsGen {
+  const $AssetsImagesIllustrationsGen();
+
+  /// File path: assets/images/illustrations/illustration_1.png
+  AssetGenImage get illustration1 =>
+      const AssetGenImage('assets/images/illustrations/illustration_1.png');
+
+  /// File path: assets/images/illustrations/illustration_2.png
+  AssetGenImage get illustration2 =>
+      const AssetGenImage('assets/images/illustrations/illustration_2.png');
+
+  /// File path: assets/images/illustrations/illustration_3.png
+  AssetGenImage get illustration3 =>
+      const AssetGenImage('assets/images/illustrations/illustration_3.png');
+
+  /// File path: assets/images/illustrations/illustration_4.png
+  AssetGenImage get illustration4 =>
+      const AssetGenImage('assets/images/illustrations/illustration_4.png');
+
+  /// File path: assets/images/illustrations/illustration_5.png
+  AssetGenImage get illustration5 =>
+      const AssetGenImage('assets/images/illustrations/illustration_5.png');
+
+  /// File path: assets/images/illustrations/illustration_6.png
+  AssetGenImage get illustration6 =>
+      const AssetGenImage('assets/images/illustrations/illustration_6.png');
+
+  /// File path: assets/images/illustrations/illustration_7.png
+  AssetGenImage get illustration7 =>
+      const AssetGenImage('assets/images/illustrations/illustration_7.png');
+
+  /// File path: assets/images/illustrations/illustration_8.png
+  AssetGenImage get illustration8 =>
+      const AssetGenImage('assets/images/illustrations/illustration_8.png');
+
+  /// List of all assets
+  List<AssetGenImage> get values => [
+        illustration1,
+        illustration2,
+        illustration3,
+        illustration4,
+        illustration5,
+        illustration6,
+        illustration7,
+        illustration8
+      ];
 }
 
 class $AssetsImagesLogoGen {
